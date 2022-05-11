4101afac8d73edf35f0b08a63dacb4c15a9bff64
icabiKrgz <70774175+icabiKrgz@users.noreply.github.com>
2022-05-11 23:33:25 +0300

    React

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Laravel 8 & React JS / Icabi Kırgız</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  </head>
+  <body>
+      <div id="index"></div>
+        <script src="{{asset('js/app.js')}}"></script>
+  </body>
+</html>
+<!-- Icabi Kırgız -->
